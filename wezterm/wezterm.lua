e3a28ada58503624e026bda6d47f6d6961714923
Tobias Newmiwaka <newmi1988@gmail.com>
2024-01-24 21:58:02 +0100

    refactor: restructure config
    - add update function for table

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,30 @@
 local wezterm = require('wezterm')
 
-return {
+local function update(first_table, second_table)
+    for k, v in pairs(second_table) do
+        first_table[k] = v
+    end
+    return first_table
+end
+
+local config = {}
+
+local appereance = {
     enable_tab_bar = false,
     window_decorations = "RESIZE",
+    window_background_opacity = 0.9375,
     color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+}
+
+local keys = {
+    {
+        key = 'q',
+        mods = 'CMD',
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+}
+
+local behaviour = {
     skip_close_confirmation_for_processes_named = {
         'bash',
         'sh',
@@ -12,11 +33,12 @@ return {
         'nu',
         'zellij'
     },
-    keys = {
-        {
-            key = 'q',
-            mods = 'CMD',
-            action = wezterm.action.CloseCurrentPane { confirm = true },
-        },
-    },
+    send_composed_key_when_left_alt_is_pressed = true,
+    send_composed_key_when_right_alt_is_pressed = true,
 }
+
+config = update(config,appereance)
+config = update(config,behaviour)
+config.keys = keys
+
+return config
